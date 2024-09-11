259977d6947b992981350878dde438145d8a2a38
Salaruddin <salaruddinjalal@gmail.com>
2024-09-11 18:44:16 +0430

    Log and Message entities/models added

---
+++ backend net8/Core/Entities/Message.cs
@@ -0,0 +1,9 @@
+﻿namespace backend_net8.Core.Entities
+{
+    public class Message : BaseEntity<long>
+    {
+        public string Sender { get; set; }
+        public string Receiver { get; set; }
+        public string Text { get; set; }
+    }
+}
