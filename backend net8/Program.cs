a1938bfd087b049a552d39cd2a2332571e6d9dd5
Salaruddin <salaruddinjalal@gmail.com>
2024-09-12 01:36:25 +0430

    Injected Identity services and configure identity options in program.cs

--- backend net8/Program.cs
+++ backend net8/Program.cs
@@ -1,4 +1,6 @@
 using backend_net8.Core.ApplicationDbContext;
+using backend_net8.Core.Entities;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration.Json;
 using System.Text.Json.Serialization;
@@ -12,7 +14,27 @@ builder.Services
     .AddJsonOptions(
     options => options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
 
+//injecting database service to the application.
 builder.Services.AddDbContext<ApplicationDbContext>(optionsAction: options => options.UseSqlServer(builder.Configuration.GetConnectionString("ConnectionString1")));
+
+//add identity
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
+
+//config identity
+builder.Services.Configure<IdentityOptions>(options => {
+    options.Password.RequiredLength = 8;
+    options.Password.RequireDigit = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequireNonAlphanumeric = false;
+
+    options.SignIn.RequireConfirmedPhoneNumber = false;
+    options.SignIn.RequireConfirmedEmail = false;
+    options.SignIn.RequireConfirmedAccount = false;
+
+});
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
