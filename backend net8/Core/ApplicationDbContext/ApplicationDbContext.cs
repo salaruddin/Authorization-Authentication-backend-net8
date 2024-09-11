bd53963cbfd8f27ef1683af572d827ed5704c8db
Salaruddin <salaruddinjalal@gmail.com>
2024-09-11 18:52:29 +0430

    application db context is added

---
+++ backend net8/Core/ApplicationDbContext/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using backend_net8.Core.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend_net8.Core.ApplicationDbContext
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Log> Logs { get; set; }
+        public DbSet<Message> Messages { get; set; }
+    }
+
+}
