f8d2340e1a212123603a9d9d6119e56ca8566de8
Salaruddin <salaruddinjalal@gmail.com>
2024-09-11 15:09:36 +0430

    Add project files.

---
+++ backend net8/Core/Entities/ApplicationUser.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace backend_net8.Core.Entities
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public required string Address { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        [NotMapped]
+        public required IList<string> Roles { get; set; }
+    }
+}
---
+++ backend net8/Core/Entities/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace backend_net8.Core.Entities
+{
+    public class BaseEntity<TID>
+    {
+        public  TID MyProperty { get; set; }
+        public DateTime CreatedAt { get; set; }= DateTime.Now;
+        public DateTime LastUpdatedAt { get; set;} = DateTime.Now;
+        public bool IsActive { get; set; } = true;
+        public bool IsDeleted { get; set; }= false;
+    }
+}
