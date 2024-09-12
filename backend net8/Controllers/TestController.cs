b87ede66a68f9c34803b530f359776ef5016f4e2
Salaruddin <salaruddinjalal@gmail.com>
2024-09-12 08:42:33 +0430

    Add controllers without actions

---
+++ backend net8/Controllers/TestController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend_net8.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+    }
+}
