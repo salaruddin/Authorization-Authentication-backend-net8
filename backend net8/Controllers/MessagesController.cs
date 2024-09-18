296b465d0dd1f6f9bae90ff5a648417cc8774a6f
Salaruddin <salaruddinjalal@gmail.com>
2024-09-18 11:29:19 +0430

    Implement Message Controller

--- backend net8/Controllers/MessagesController.cs
+++ backend net8/Controllers/MessagesController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using backend_net8.Core.Constants;
+using backend_net8.Core.DTOs.Message;
+using backend_net8.Core.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace backend_net8.Controllers
@@ -7,5 +11,43 @@ namespace backend_net8.Controllers
     [ApiController]
     public class MessagesController : ControllerBase
     {
+        private readonly IMessageService _messageService;
+
+        public MessagesController(IMessageService messageService)
+        {
+            _messageService = messageService;
+        }
+
+        //create new message to send to another user
+        [HttpPost]
+        [Route("Create")]
+        [Authorize]
+        public async Task<IActionResult> CreateNewMessage([FromBody] CreateMessageDto createMessageDto)
+        {
+            var result = await _messageService.CreateMessageAsync(User, createMessageDto);
+            if (result.IsSucceed)
+                return Ok(result.Message);
+
+            return StatusCode(result.StatusCode, result.Message);
+        }
+
+        [HttpGet]
+        [Route("mine")]
+        [Authorize]
+        public async Task<ActionResult<IEnumerable<GetMessageDto>>> GetMyMessage()
+        {
+            var messages = await _messageService.GetMyMessagesAsync(User);
+             return Ok(messages);
+        }
+
+
+        [HttpGet]
+        [Authorize(Roles = StaticUserRoles.OwnerAdmin)]
+        public async Task<ActionResult<IEnumerable<GetMessageDto>>> GetMessages()
+        {
+            var messages = _messageService.GetMessagesAsync();
+            return Ok(messages); 
+        }
+
     }
 }
