081b5301b8a773a493a04a87200e7c47a607b724
Salaruddin <salaruddinjalal@gmail.com>
2024-09-18 10:50:53 +0430

    Implement LogsController

--- backend net8/Controllers/LogsController.cs
+++ backend net8/Controllers/LogsController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using backend_net8.Core.Constants;
+using backend_net8.Core.DTOs.Log;
+using backend_net8.Core.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace backend_net8.Controllers
@@ -7,5 +11,28 @@ namespace backend_net8.Controllers
     [ApiController]
     public class LogsController : ControllerBase
     {
+        private readonly ILogService logService;
+
+        public LogsController(ILogService logService)
+        {
+            this.logService = logService;
+        }
+
+        [HttpGet]
+        [Authorize(Roles =StaticUserRoles.OwnerAdmin)]
+        public async Task<ActionResult<IEnumerable<GetLogDto>>> GetLogs()
+        {
+            var logs = await logService.GetLogsAsync();
+            return Ok(logs);
+        }
+
+        [HttpGet]
+        [Route("mine")]
+        [Authorize]
+        public async Task<ActionResult<IEnumerable<GetLogDto>>> GetMyLogs()
+        {
+            var logs = await logService.GetMyLogsAsync(User);
+            return Ok(logs); 
+        }
     }
 }
